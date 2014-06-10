f315f53d8d4718bebee7a2a60375b960b1913162
Andrew Weller <Andrew.Weller@soteradefense.com>
2014-06-10 09:09:49 -0400

    Added uniform layout and file manager menus, then implemented them in the graph toolbars

--- graphene-enron/graphene-enron-web/src/main/webapp/TransactionsGraph/js/TXN_GraphToolBar.js
+++ graphene-enron/graphene-enron-web/src/main/webapp/TransactionsGraph/js/TXN_GraphToolBar.js
@@ -9,140 +9,16 @@ Ext.define("DARPA.TXNGToolbar", {  // allows the user to change the graph layout
 		'-',
 		{
 		    text: 'File',
-		    menu: Ext.create("Ext.menu.Menu", {
-			//style: { backgroundColor: "FloralWhite", fontSize: "medium" }, 
-			items: [
-			    {
-				text: "EXPORT GRAPH",
-				handler: function() {
-				    var gf = getTransactionGraphFrame();
-				    var gr = gf.getGraph();                             
-				    if (gr) {
-					gr.exportGraph();
-
-					// DRAPER API - this one uses the workflow type of REPORT
-					// Send a User Activity Message with optional metadata
-					//activityLogger.logUserActivity('Export Graph', '<graph tab name>', activityLogger.WF_REPORT, 
-					//    {'Tab':'<graph tab>', 'action': 'Export', 'selectedNode': 'NA' });
-				    }
-				}
-			    }, 
-			    {
-				text: "IMPORT GRAPH",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.exportGraph();
-				    Ext.Msg.confirm('Confirm', 
-					 'Importing another graph will delete the current graph. Are you sure you want to Import?', 
-					  function(ans) {
-					      if (ans == 'yes') {
-						  self.importGraph();
-					      }
-					    }
-					);
-				}
-			    }, 
-			    {
-				text: "SAVE GRAPH",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-                                    var toolbar = menu.up().up();
-                                    var gr = toolbar.up();
-                                    gr.saveGraph();
-
-					// DRAPER API - this one uses the workflow type of REPORT
-					// Send a User Activity Message with optional metadata
-					//activityLogger.logUserActivity('Save Graph', '<graph tab name>', activityLogger.WF_REPORT, 
-					//    {'Tab':'Transaction Graph', 'action': 'Save', 'selectedNode': 'NA' });
-				    }
-			    }, 
-			    {
-				text: "RESTORE GRAPH",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-                                    var toolbar = menu.up().up();
-                                    var gr = toolbar.up();
-                                    Ext.Msg.confirm('Confirm', 
-                                        'Restoring a saved graph will delete the current graph. Are you sure you want to Restore?', 
-                                        function(ans) {
-                                            if (ans == 'yes') {
-                                                gr.restoreGraph(null);
-                                            }
-                                        }
-                                    ); // msg
-				}
-			    },
-
-			] // items
-		    } // menu items
-		    ) // create menu
+		    menu: Ext.create("DARPA.GraphManagerMenu", {
+				style: { backgroundColor: "FloralWhite", fontSize: "medium" }
+			})
 		},
 		'-',
 		{
 		    text: 'Change View',
-		    menu: Ext.create("Ext.menu.Menu", {
-			style: { backgroundColor: "FloralWhite", fontSize: "medium" }, 
-			items: [
-			    {
-				text: "CIRCULAR",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('circle');
-				}
-			    }, 
-			    {
-				text: "GRID",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('grid');
-				}
-			    }, 
-			    {
-				text: "HIERARCHICAL",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('breadthfirst');
-				}
-			    }, 
-			     {
-				text: "FORCE DIRECTED - Wheel",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('arbor-wheel');
-				}
-			    },
-			    {
-				text: "FORCE DIRECTED - Snowflake",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('arbor-snow');
-				}
-			    },
-			    {
-				text: "FORCE DIRECTED - COSE",
-				handler: function(item) {
-				    var menu = item.parentMenu;
-				    var toolbar = menu.up().up();
-				    var gr = toolbar.up();
-				    gr.GraphVis.changeLayout('cose');
-				}
-			    }
-			    // 'random' layout is also available but pretty useless so this one is omitted
-			] // items
-		    } // menu items
-		    ) // create menu
+		    menu: Ext.create("DARPA.GraphLayoutMenu", {
+				style: { backgroundColor: "FloralWhite", fontSize: "medium" }
+			})
 		},
 		'-',    // separator
 		// button to refresh the display
